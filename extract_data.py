e270dcc3be92d870b1606c7c2a69b64aefd2426a
Mark Riedesel <mark.riedesel@gmail.com>
2014-05-09 10:37:05 -0400

    Initial commit

---
+++ extract_data.py
@@ -0,0 +1,72 @@
+# This program is designed to read in the IPEDS file on higer ed instutution data and 
+# return a subset of interest for plotting retention data as a function of 
+# tuition and Pell grant per institution.  It can easily be adapted to return other 
+# fields as well.
+#
+#
+# Mark Riedsel, April 29, 2014
+#
+# The information on the fields in the input CSV file is contained in the 
+# "Delta Data Dictory 1987_2010.xls"
+#
+
+import sys
+import argparse
+import string
+import os
+import csv
+
+# Read in the name for the input file and the output file from the command line
+parser = argparse.ArgumentParser()
+parser.add_argument("filein", help="Type the name of the input text file here")
+parser.add_argument("fileout", help="Type the name of the output file here")
+parser.add_argument("yearin", help="Type the year for the data to collect here")
+args = parser.parse_args()
+yearin = args.yearin
+file_out = open(args.fileout,'w')
+
+cnt = 0
+with open(args.filein,'r') as input_file:
+    for line in input_file:
+        cnt = cnt + 1
+        words = line.split(',')
+        if cnt == 1:
+            for index in range(len(words)):
+                words[index] = words[index].strip('"')
+        year = words[1]
+        institution = words[5].strip('"')
+        city = words[7].strip('"')
+        state = words[8].strip('"')
+        zipcode = words[9].strip('"')
+        insttype = words[12]
+        print insttype
+        carnegie2000 = words[19]
+        enrollment = words[44]
+        pellgrants = words[94]
+        tuition = words[123]
+        gradrate_p4yr = words[282]
+        try: 
+            if cnt == 1:
+                file_out.write(year + "," + institution + "," + city + "," + state + "," + zipcode + "," + insttype + \
+                   "," + carnegie2000 + "," + enrollment + "," + pellgrants + "," + tuition + "," + gradrate_p4yr + "\n")
+            if cnt > 1 and tuition != ""  \
+                       and gradrate_p4yr != "" \
+                       and year == yearin \
+                       and float(enrollment) >= 1000. \
+                       and int(insttype) <= 3 :
+                if int(insttype) == 1 :
+                    insttype = "Public"
+                elif int(insttype) == 2 :     
+                    insttype = "Private Not-For-Profit"
+                elif int(insttype) == 3 :
+                    insttype = "Private For-Profit"
+                else:
+                    print "This should never occur, something is wrong"
+                gradrate = str(float(gradrate_p4yr)*100.)
+                file_out.write(year + "," + institution + "," + city + "," + state + "," + zipcode + "," + insttype + \
+                   "," + carnegie2000 + "," + enrollment + "," + pellgrants + "," + tuition + "," + gradrate + "\n")
+        except ValueError:
+            pass
+        finally:
+            pass
+    file_out.close()
