da737faf8e7c77abf037e0a9dff2f38ee4c7b5ec
Mark Riedesel <mark.riedesel@gmail.com>
2014-05-13 21:45:14 -0400

    Added comments and refreshed index.html

--- extract_data.py
+++ extract_data.py
@@ -22,7 +22,6 @@ getcontext().prec = 1
 # Read in the name for the input file and the output file from the command line
 parser = argparse.ArgumentParser()
 parser.add_argument("filein", help="Type the name of the input text file here")
-# parser.add_argument("fileout", help="Type the name of the output file here")
 parser.add_argument("yearin", help="Type the year for the data to collect here")
 args = parser.parse_args()
 yearin = args.yearin
@@ -37,27 +36,39 @@ with open(args.filein,'r') as input_file:
         if cnt == 1:
             for index in range(len(words)):
                 words[index] = words[index].strip('"')
+        # Academic Year 
         year = words[1]
+        # Institution Name
         institution = words[5].strip('"')
         city = words[7].strip('"')
         state = words[8].strip('"')
         zipcode = words[9].strip('"')
-        insttype = words[12]
+        # Institution Type: 1 = Public 4 yr, 2 = Private Not-for-Profit 4 yr, 3= Private for-Profit 4yr
+        insttype = words[12
         carnegie2000 = words[19]
+        # 12 Month Enrollment
         enrollment = words[44]
+        # Total Annual Pell Grants
         pellgrants = words[94]
+        # The so-called "Sticker Price"
         tuition = words[123]
+        # 6 year graduation rate
         gradrate_p4yr = words[282]
         try: 
             if cnt == 1:
+            # Write out the header with the field names as the first line
                 file_out.write(year + "," + institution + "," + city + "," + state + "," + zipcode + "," + insttype + \
                    "," + carnegie2000 + "," + enrollment + "," + pellgrants + "," + tuition + "," + gradrate_p4yr + "\n")
+            # Filter the output data to tuition and graduation year not blank
+            # Enrollment > 800,  Graduation Rate not exactly 100%,
+            # and just 4 year institutions (insttype <= 3)
             if cnt > 1 and tuition != ""  \
                        and gradrate_p4yr != "" \
                        and year == yearin \
                        and float(enrollment) >= 800. \
                        and float(gradrate_p4yr) < .999 \
                        and int(insttype) <= 3 :
+                # Write out a descriptive string instead of a number for Institution Type       
                 if int(insttype) == 1 :
                     insttype = "Public"
                 elif int(insttype) == 2 :     
@@ -66,11 +77,13 @@ with open(args.filein,'r') as input_file:
                     insttype = "Private For-Profit"
                 else:
                     print "This should never occur, something is wrong"
+                # make the graduation rate a percentage    
                 gradrate = str(float(gradrate_p4yr)*100.)
                 enrollment = str(int(round(float(enrollment))))
                 pellgrants = str(int(round(float(pellgrants))))
                 file_out.write(year + "," + institution + "," + city + "," + state + "," + zipcode + "," + insttype + \
                    "," + carnegie2000 + "," + enrollment + "," + pellgrants + "," + tuition + "," + gradrate + "\n")
+        # Just skip any lines that cause trouble        
         except ValueError:
             pass
         finally:
